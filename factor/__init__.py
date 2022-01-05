32aac42308935bab951e34780a8710e9f9fb5233
Wang Zixiong <zixiongw@163.com>
2022-01-06 00:32:42 +0800

    获取计算好的因子数据，整理为标准格式输出

---
+++ factor/__init__.py
@@ -0,0 +1,11 @@
+# getFactorInfo：整理因子为相同格式：因子名称，因子类型，因子计算方式，因子值
+# slicefactor：根据股票池、时间等限制对factor matrix进行切片
+# preprocessing：对于因子切片数据进行预处理
+# a.遮罩：剔除上市未满一年、ST、涨跌停的股票等
+# b.极值处理、中性化与标准化处理等
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  4 15:20:00 2022
+@author: Zixiong Wang
+"""
+from .loadData import *

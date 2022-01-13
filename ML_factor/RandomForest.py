fdf5f306e75fb02a6ef99fcef98e8bcab8ab65a6
Wang Zixiong <zixiongw@163.com>
2022-01-13 10:10:40 +0800

    ——

--- ML_factor/RandomForest.py
+++ ML_factor/RandomForest.py
@@ -66,36 +66,69 @@ class RF():
             pickle.dump(self.Y, file)
 
     def data_preparation(self):
-        if os.path.exists(rootPath+r'\data\RFData_feature.pkl') and os.path.exists(rootPath+r'\data\RFData_label.pkl'):
+        if os.path.exists(rootPath+r'\data\RFData_feature.pkl'):
             self.X = pd.read_pickle(rootPath+r'\data\RFData_feature.pkl')
-            self.Y = pd.read_pickle(rootPath+r'\data\RFData_label.pkl')
         else:
             self.X_preperation()
+        if os.path.exists(rootPath + r'\data\RFData_label.pkl'):
+            self.Y = pd.read_pickle(rootPath + r'\data\RFData_label.pkl')
+        else:
             self.Y_preperation()
+
+    def train_test_split_function(self,trainStartDateInd,trainEndDateInd,testStartDateInd,testEndDateInd):
+        # 切片取数，整理为一维的输入输出
+        X_raw_train = self.X[:,trainStartDateInd:trainEndDateInd,:]
+        X_train = X_raw_train.reshape([np.shape(X_raw_train)[0] * np.shape(X_raw_train)[1], np.shape(X_raw_train)[2]])
+        y_train = self.Y[:,trainStartDateInd:trainEndDateInd].flatten()
+
+        X_raw_test = self.X[:, testStartDateInd:testEndDateInd, :]
+        X_test = X_raw_test.reshape([np.shape(X_raw_test)[0] * np.shape(X_raw_test)[1], np.shape(X_raw_test)[2]])
+        y_test = self.Y[:, testStartDateInd:testEndDateInd].flatten()
+
+        # 对于label为0的样本直接剔除
+        available_y_train_Index = np.isnan(y_train)
+        y_train = y_train[available_y_train_Index == False]
+        X_train = X_train[available_y_train_Index == False]
+
+        # 树模型需要非空的输入输出，需要将NaN的样本feature和label填充为0
+        np.nan_to_num(X_train, copy=False)
+        np.nan_to_num(y_train, copy=False)
+
+        available_y_test_Index = np.isnan(y_test)
+        y_test = y_test[available_y_test_Index == False]
+        X_test = X_test[available_y_test_Index == False]
+        np.nan_to_num(X_test, copy=False)
+        np.nan_to_num(y_test, copy=False)
+        return {'X_train':X_train,'y_train':y_train,'X_test':X_test,'y_test':y_test}
+
+    def whole_sample_fit(self,MaxDepth,N_Estimators):
+        self.get_tradedays()
+        model = RandomForestRegressor(criterion = 'mse',n_estimators=N_Estimators,max_depth = MaxDepth)
+        trainStartDateInd, trainEndDateInd = 0 , int(np.shape(self.X)[1]*0.8)
+        testStartDateInd, testEndDateInd = int(np.shape(self.X)[1]*0.8),np.shape(self.X)[1]
+        train_test_dataDict = self.train_test_split_function(trainStartDateInd, trainEndDateInd, testStartDateInd,testEndDateInd)
+        X_train, y_train, X_test, y_test = train_test_dataDict['X_train'], train_test_dataDict['y_train'], \
+                                           train_test_dataDict['X_test'], train_test_dataDict['y_test']
+
+        model.fit(X_train, y_train)
+        y_predict = model.predict(X_test)
+        mse = metrics.mean_squared_error(y_test, y_predict)
+
+        print("startDate = {},endDate = {}, mse={}".format(self.all_date[trainStartDateInd],self.all_date[testStartDateInd], mse))
+
     def rolling_fit(self):
         self.get_tradedays()
         model = RandomForestRegressor(criterion = 'mse',max_depth= 4)
         for step in tqdm(range((len(self.all_date)-self.batch_size)//self.rolling_step+1)):
-            trainStartDateInd,trainEndDateInd = step*self.rolling_step,self.batch_size+step*self.rolling_step
+            trainStartDateInd, trainEndDateInd = step*self.rolling_step,self.batch_size+step*self.rolling_step
             testStartDateInd, testEndDateInd = self.batch_size+step * self.rolling_step, min(self.batch_size + (step+1) * self.rolling_step,self.T)
 
-            # 调整矩阵使得模型的输入数据为一维数据
-            X_raw_train = self.X[:,trainStartDateInd:trainEndDateInd,:]
-            X_train = X_raw_train.reshape([np.shape(X_raw_train)[0]*np.shape(X_raw_train)[1],np.shape(X_raw_train)[2]])
-            y_train = self.Y[:,trainStartDateInd:trainEndDateInd].flatten()
-
-            X_raw_test = self.X[:, testStartDateInd:testEndDateInd, :]
-            X_test = X_raw_test.reshape([np.shape(X_raw_test)[0] * np.shape(X_raw_test)[1], np.shape(X_raw_test)[2]])
-            y_test = self.Y[:, testStartDateInd:testEndDateInd].flatten()
-
-            # 树模型需要非空的输入输出，因此需要将NaN的样本feature和label填充为0
-            np.nan_to_num(X_train,copy = False)
-            np.nan_to_num(y_train,copy = False)
-            np.nan_to_num(X_test,copy = False)
-            np.nan_to_num(y_test,copy = False)
-
+            train_test_dataDict =self.train_test_split_function(trainStartDateInd, trainEndDateInd, testStartDateInd, testEndDateInd)
+            X_train,y_train,X_test,y_test = train_test_dataDict['X_train'],train_test_dataDict['y_train'],\
+                                            train_test_dataDict['X_test'],train_test_dataDict['y_test']
 
             model.fit(X_train,y_train)
             y_predict = model.predict(X_test)
-            accuracy = metrics.mean_squared_error(y_test,y_predict)
-            print("step={}, startDate = {},endDate = {}, accuracy={}".format(step, self.all_date[trainStartDateInd], self.all_date[testStartDateInd],accuracy))
+            mse = metrics.mean_squared_error(y_test,y_predict)
+            print("step={}, startDate = {},endDate = {}, mse={}".format(step, self.all_date[trainStartDateInd], self.all_date[testStartDateInd],mse))
+            return float(format(mse,'.2g'))

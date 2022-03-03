82b0ca21cbde955f08163fb59673ab56e4bfc294
Wang Zixiong <zixiongw@163.com>
2022-03-03 08:59:36 +0800

    LGBM因子合成

---
+++ test/test_LGBM.py
@@ -0,0 +1,13 @@
+"""
+@Author: Wang
+@Time: 2022/3/2 11:49
+@SoftWare: PyCharm
+@File: test_KNN.py
+"""
+from ML_factor.LGBM import LGBM
+rootPath = 'C:\\Users\\Lenovo\\Desktop\\毕设材料\\PerformanceAttributionViaMachineLearning\\'
+
+lgbm = LGBM('open')
+lgbm.data_preparation()
+factorExposure = lgbm.rolling_fit()
+factorExposure.to_csv(rootPath+'factor\\LGBM因子载荷矩阵.csv')

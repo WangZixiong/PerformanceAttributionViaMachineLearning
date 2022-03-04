5cd84e870ed8ea02f9469179235fab5730e470d6
Wang Zixiong <zixiongw@163.com>
2022-03-04 09:59:19 +0800

    修改了因子文件，取alpha1-alpha50,

--- data/basicData.py
+++ data/basicData.py
@@ -5,11 +5,12 @@
 @Software: PyCharm
 """
 import pandas as pd
-rootPath = 'C:\\Users\\Lenovo\\Desktop\\毕设材料\\PerformanceAttributionViaMachineLearning\\'
 class BasicData:
+    rootPath = 'C:\\Users\\Lenovo\\Desktop\\毕设材料\\PerformanceAttributionViaMachineLearning\\'
     with open(rootPath+r'data\2011-2020年个股开盘价与复权因子.csv', 'rb') as f1:
         basicMkt = pd.read_csv(f1, index_col=0)
-    with open(rootPath+r'data\pickleFactors_40factor_gtja191.pickle', 'rb') as f2:
+        # basicMkt = pd.read_pickle(f1)
+    with open(rootPath+r'data\seperateData\pickleFactors_01_50_gtja191.pickle', 'rb') as f2:
         basicFactor = pd.read_pickle(f2)
 
     def __new__(cls):

0f5b0cf4a9d8fff6a678d91ab7587d46d04f05a4
Wang Zixiong <61180828+WangZixiong@users.noreply.github.com>
2022-03-07 09:28:18 +0800

    KNN修改

--- ML_factor/KNN.py
+++ ML_factor/KNN.py
@@ -8,6 +8,8 @@ import torch
 import itertools
 import math
 import tqdm
+import pickle
+import time
 import pandas as pd
 import numpy as np
 import torch.nn as nn
@@ -44,15 +46,17 @@ class KNNModel(nn.Module):
 
 class KNN:
     def __init__(self):
-        self.step = 1
+        self.step = 10
         self.val_proportion = 0.2
-        self.rolling_size = 201
+        self.rolling_size = 200 + self.step
 
     def data_load(self):
         self.X = pd.read_pickle('./data/cleanData/cleanFactors_gtja191_20220204.pkl')
         self.Y = pd.read_pickle('./data/cleanData/y_prediction.pkl')
+        self.X = self.X[:, :-2, :]
+        self.Y = self.Y[:, 2:]
 
-    def init_model(self, neu_range=[32, 64, 128], fc_range=[1, 2, 3]):
+    def init_model(self, neu_range=[64], fc_range=[1, 2, 3, 4, 5]):
         self.model_list = list()
         para_list = list(itertools.product(neu_range, fc_range))
         for para in para_list:
@@ -62,10 +66,11 @@ class KNN:
 
     def get_rolling_data(self, r):
         if r * self.step + self.rolling_size > self.X.shape[1]:
-            X = self.X[:, r * self.step:-1, :]
+            X = self.X[:, r * self.step:, :]
+            Y = self.Y[:, r * self.step:]
         else:
             X = self.X[:, r * self.step:r * self.step + self.rolling_size, :]
-        Y = self.Y[:, r * self.step+1:r * self.step + self.rolling_size+1]
+            Y = self.Y[:, r * self.step:r * self.step + self.rolling_size]
         X = torch.Tensor(X)
         X = torch.where(torch.isinf(X), torch.full_like(X, 0), X)
         Y = torch.Tensor(Y)
@@ -75,27 +80,27 @@ class KNN:
         self.data_load()
         self.init_model()
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # device = torch.device("cpu")
-        for r in range(math.ceil((self.Y.shape[1]-self.rolling_size-1)/self.step)):
-        # for r in range(2):
-            print(f'\n Rolling step:{r} ')
-            X, Y = self.get_rolling_data(r)
-            X_test = X[:, -1, :]
-            Y_test = Y[:, -1].flatten().unsqueeze(1)
-            X = X[:, :-1, :].flatten(0, 1)
-            Y = Y[:, :-1].flatten().unsqueeze(1)
-            dataset = TensorDataset(X, Y)
-            train_set, val_set = torch.utils.data.random_split(dataset, [round(Y.shape[0]*(1-self.val_proportion)),
-                                                                         round(Y.shape[0]*self.val_proportion)])
-            train_loader = DataLoader(train_set, shuffle=True, batch_size=50)
-            val_loader = DataLoader(val_set, shuffle=True, batch_size=50)
-            for i, model in enumerate(self.model_list):
+        for i, model in enumerate(self.model_list):
+            print(f'model{i}')
+            for r in range(math.ceil((self.Y.shape[1]-self.rolling_size-1)/self.step)):
+                print(f'\n Rolling step:{r} ')
+                X, Y = self.get_rolling_data(r)
+                X_test = X[:, -self.step:, :].flatten(0, 1)
+                Y_test = Y[:, -self.step:].flatten().unsqueeze(1)
+                X = X[:, :-self.step, :].flatten(0, 1)
+                Y = Y[:, :-self.step].flatten().unsqueeze(1)
+                dataset = TensorDataset(X, Y)
+                train_set, val_set = torch.utils.data.random_split(dataset, [round(Y.shape[0]*(1-self.val_proportion)),
+                                                                             round(Y.shape[0]*self.val_proportion)])
+                train_loader = DataLoader(train_set, shuffle=True, batch_size=400)
+                val_loader = DataLoader(val_set, shuffle=True, batch_size=400)
                 model.to(device)
                 criterion = nn.MSELoss()
-                optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+                optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
                 best_loss = 1e9
-                # for epoch in range(100):
-                for epoch in range(2):
+                start_time = time.time()
+                for epoch in range(100):
+                # for epoch in range(2):
                     # model training
                     model.train()
                     train_loss = list()
@@ -119,33 +124,31 @@ class KNN:
                         valid_loss.append(loss.item())
 
                     print(f'Epoch {epoch} \t\t Training Loss: {np.nanmean(train_loss)} \t\t '
-                          f'Validation Loss: {np.nanmean(valid_loss)}')
+                          f'Validation Loss: {np.nanmean(valid_loss)} \t\t'
+                          f'Time: {time.time()-start_time}'
+                          )
 
                     # set early stop
                     if np.nanmean(valid_loss) < best_loss:
                         best_loss = np.nanmean(valid_loss)
                         es = 0
-                        # torch.save(model.state_dict(), "model_" + str(fold) + 'weight.pt')
                     else:
                         es += 1
                         print("Counter {} of 3".format(es))
                         if es > 2:
                             print("Early stopping with best_loss: ", best_loss,
                                   "and val_loss for this epoch: ", np.nanmean(valid_loss))
-                            continue
+                            break
 
-                if i == 0:
-                    best_model, best_score = model, best_loss
-                elif best_score > best_loss:
-                    best_model, best_score = model, best_loss
+                prediction = model(X_test)
+                test_loss = criterion(prediction, Y_test)
+                print(f' Model:{i} Rolling:{r} Loss on test set:{test_loss}')
 
-                pred = best_model(X_test.to(device))
-                test_loss = criterion(pred, Y_test.to(device))
-                print(f'Rolling:{r} Model:{i} Loss on test set:{test_loss}')
+                if r == 0:
+                    predictions = prediction.detach().numpy()
+                else:
+                    predictions = np.hstack((predictions, prediction.detach().numpy()))
 
-            if r == 0:
-                predictions = pred.T
-            else:
-                predictions = torch.row_stack((predictions, pred.T))
-
-        return predictions
+            torch.save(model, f"./data/model/KNN{i+1}.pt")
+            with open(f'./data/prediction/KNN{i+1}.pkl', 'wb') as file:
+                pickle.dump(predictions, file)
--- test/test_KNN.py
+++ test/test_KNN.py
@@ -3,6 +3,7 @@
 @Time: 2022/2/4 16:49
 @SoftWare: PyCharm
 @File: test_KNN.py
+@coding=gbk
 """
 from ML_factor.KNN import KNN
 

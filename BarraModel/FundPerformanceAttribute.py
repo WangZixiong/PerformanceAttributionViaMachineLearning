4064f919f7a969a42391cce2a57ea9325f8a25f8
Wang Zixiong <zixiongw@163.com>
2022-04-24 23:15:43 +0800

    业绩归因代码

--- BarraModel/FundPerformanceAttribute.py
+++ BarraModel/FundPerformanceAttribute.py
@@ -7,6 +7,7 @@ Created on Wen Apr 15 18:02:51 2022
 import sklearn.linear_model
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 import pickle
 import os
 from tqdm import tqdm
@@ -18,17 +19,20 @@ ridgeparams = {
     'alpha':0.0001
 }
 class FundPerformanceAttribute():
-    def __init__(self,barra_ML_Return,rawFundNAV):
+    def __init__(self,barra_ML_Return,rawFundNAV,year,fund):
         # 第一件事，需要筛选excel中和barra+ML因子时间段重合的基金
-        # 第二件事，需要将因子收益率的日度收益按照私募基金的日期转化为周度收益
+        # 第二件事，需要将因子收益率的日度收益按照私募基金的日期转化为对应时间点之间的收益
         # 第三件事，需要用ridge进行回归
 
         # rawFundNAV默认格式为Series
         self.allTime = [time for time in barra_ML_Return.index if time in rawFundNAV.index]
         self.barra_ML_Return = barra_ML_Return.loc[self.allTime,:]
         self.rawFundNAV = rawFundNAV[self.allTime]
+        self.year = year
+        self.fund = fund
+
     def getAdjustedBarraMLReturn_NAVReturn(self,periodTimeList):
-        # 计算每段时间内的因子收益率与基金净值收益率并返回
+        # 计算在timeList中每段时间内的因子收益率与基金净值收益率并返回
         PeriodNAVReturn = pd.DataFrame(index = periodTimeList[1:],columns=['NAVReturn'])
         PeriodFactorReturn = pd.DataFrame(index = periodTimeList[1:])
         for timeInd in range(1,len(periodTimeList)):
@@ -37,7 +41,7 @@ class FundPerformanceAttribute():
             lastNAVTimeInd,currNAVTimeInd = self.allTime.index(lastNAVTime),self.allTime.index(currNAVTime)
 
             if currNAVTimeInd - lastNAVTimeInd > 1:
-            # 拉出上个净值所在日到这个净值所在日之间的所有因子收益率数据，累乘求期间收益率
+            # 拉出从上个净值所在日到这个净值所在日之间的所有因子收益率数据，累乘求期间收益率
                 currPeriodFactor = self.barra_ML_Return.iloc[lastNAVTimeInd:currNAVTimeInd,:]
                 for factor in currPeriodFactor.columns:
                     periodFactorReturn[factor] = (1+currPeriodFactor.loc[:,factor]).cumprod()[-1]-1
@@ -48,37 +52,73 @@ class FundPerformanceAttribute():
         return PeriodFactorReturn,PeriodNAVReturn
 
     def getFundFactorExposure(self,currFactorPeriodReturn, currFundNAVPeriodReturn):
+        # 用sklearn里面的ridge model拟合
         model = sklearn.linear_model.Ridge(**ridgeparams)
         reg = model.fit(currFactorPeriodReturn, currFundNAVPeriodReturn)
         FundFactorExposure, FundAlpha, score = reg.coef_, reg.intercept_, reg.score(currFactorPeriodReturn,
                                                                                          currFundNAVPeriodReturn)
-        return [FundFactorExposure, FundAlpha, score]
+        # 用statsmodels中的sm.OLS拟合，便于看每个变量的t值
+        currFactorPeriodReturn = sm.add_constant(currFactorPeriodReturn)
+        model = sm.OLS(np.array(currFundNAVPeriodReturn).astype(float),np.array(currFactorPeriodReturn).astype(float))
+        results = model.fit()
+        FundFactorExposure, FactorTValue, FactorPValue, score = results.params.astype(float),results.tvalues.astype(float),results.pvalues.astype(float),results.rsquared
+        return [FundFactorExposure, FactorTValue, FactorPValue, score, results]
+    def getExcessReturn(self):
+        # 0423 按照年份取基金净值时间区间
+        if self.year != 0:
+            currYearTime = [i for i in self.allTime if i.year == self.year]
+            currYearLastTimeInd = self.allTime.index(currYearTime[-1])
+            untilCurrYearTime = self.allTime[:currYearLastTimeInd]
+        else:
+            untilCurrYearTime = self.allTime
+        # 按照基金净值的时间点将时间分成一段一段的
+        fundAvailableTime = []
+        for time in untilCurrYearTime:
+            if self.rawFundNAV[time] > 0:
+                fundAvailableTime.append(time)
+        if len(fundAvailableTime) >30:
+            startNAVTime, endNAVTime = fundAvailableTime[1], fundAvailableTime[-1]
+            PeriodFactorReturn, PeriodNAVReturn = self.getAdjustedBarraMLReturn_NAVReturn(fundAvailableTime)
+            PeriodFactorReturn.fillna(0,inplace=True)
+            FundFactorExposure, FactorTValue, FactorPValue, score,results = self.getFundFactorExposure(PeriodFactorReturn, PeriodNAVReturn)
+            alphaDF = pd.DataFrame(index = fundAvailableTime[1:])
+            for time in alphaDF.index:
+                timeInd = fundAvailableTime.index(time)-1
+                alphaDF.loc[time,'alpha'] = results.resid[timeInd]
+            return results,alphaDF
+        else:
+            print(str(self.year)+'年'+self.fund+'净值数据过少')
+            return 0
+
+
     def getRollingFundExposure(self,lookbackPeriod):
         # 在本函数中，按照基金净值的时间点将时间分成一段一段的
         fundAvailableTime = []
         for time in self.allTime:
             if self.rawFundNAV[time] > 0:
                 fundAvailableTime.append(time)
-
         # 设定rolling长度
         if lookbackPeriod == '季度':
             lookbackLength = 90
         elif lookbackPeriod == '半年度':
             lookbackLength = 180
+        elif lookbackPeriod == '年度':
+            lookbackLength = 360
 
         startNAVTime,endNAVTime = fundAvailableTime[1],fundAvailableTime[-1]
         if (endNAVTime-startNAVTime).days < lookbackLength+30:
             print('基金净值的时间跨度过短，无法进行业绩归因')
-            return 'error','error'
+            return 'error','error','error','error'
         else:
             # 记录滚动风险暴露与MLFactor暴露
-            AllPeriodFundFactorExposure, AllPeriodScore = pd.DataFrame(),pd.DataFrame()
+            AllPeriodFundFactorExposure, AllPeriodFactorTValue, AllPeriodFactorPValue, AllPeriodScore = pd.DataFrame(),pd.DataFrame(),pd.DataFrame(),pd.DataFrame()
+            AllPeriodMLFactorAttributionPercent = pd.DataFrame()
 
             # 滚动长度由输入参数决定，滚动频率是一个月一滚动
             # 计算滚动次数
             firstRollingEndTime = startNAVTime+pd.Timedelta(days=lookbackLength)
             rollingMonthAmount = endNAVTime.year*12+endNAVTime.month - firstRollingEndTime.year*12-firstRollingEndTime.month + 1
-            wholeStartYear,wholeStartMonth, wholeEndYear, wholeEndMonth = startNAVTime.year,startNAVTime.month,endNAVTime.year,endNAVTime.month
+            wholeStartYear, wholeStartMonth, wholeEndYear, wholeEndMonth = startNAVTime.year,startNAVTime.month,endNAVTime.year,endNAVTime.month
 
             for rollingNum in tqdm(range(rollingMonthAmount)):
                 # 如果是第一个周期，则开始时间为第一个净值收益率时间（这里确认一下是净值收益率时间，不是净值时间！！！！！！！！！）
@@ -92,7 +132,9 @@ class FundPerformanceAttribute():
                     startTimeYear,startTimeMonth = startTimeYearMonth//12,startTimeYearMonth%12
                     if startTimeMonth == 0:
                         startTimeYear, startTimeMonth = startTimeYear-1,12
+                    # 提取本月的所有净值数值不为0的日期
                     startTimeMonthNAVTimeList = [time for time in fundAvailableTime if time.year == startTimeYear and time.month == startTimeMonth]
+                    # 如果本月没有不为0的日期，就用最一开始的日期作为开始日期，有点鲁莽但保证能运行出来
                     if startTimeMonthNAVTimeList == []:
                         periodStartTime, periodStartTimeInd = startNAVTime, 0
                     else:
@@ -108,10 +150,11 @@ class FundPerformanceAttribute():
                 else:
                     endTimeYearMonth = wholeEndYear * 12 + wholeEndMonth + (rollingNum - rollingMonthAmount)
                     endTimeYear, endTimeMonth = endTimeYearMonth // 12, endTimeYearMonth % 12
-                    if endTimeYearMonth == 0:
+                    if endTimeMonth == 0:
                         endTimeYear, endTimeMonth = endTimeYear-1, 12
                     endTimeMonthNAVTimeList = [time for time in fundAvailableTime if
                                                  time.year == endTimeYear and time.month == endTimeMonth]
+                    # 如果本月没有不为0的日期，就用最后一个日期作为结束日期，有点鲁莽但保证能运行出来
                     if endTimeMonthNAVTimeList == []:
                         periodEndTime, periodEndTimeInd = endNAVTime,len(fundAvailableTime)-1
                     else:
@@ -120,9 +163,11 @@ class FundPerformanceAttribute():
 
                     periodTimeList = fundAvailableTime[periodStartTimeInd:periodEndTimeInd+1]
                     PeriodFactorReturn,PeriodNAVReturn = self.getAdjustedBarraMLReturn_NAVReturn(periodTimeList)
-                    [FundFactorExposure, FundAlpha, score] = self.getFundFactorExposure(PeriodFactorReturn, PeriodNAVReturn)
+                    [FundFactorExposure, FactorTValue, FactorPValue, score, results] = self.getFundFactorExposure(PeriodFactorReturn, PeriodNAVReturn)
                 ind = str(startTimeYear*100+startTimeMonth)+'-'+str(endTimeYear*100+endTimeMonth)
-                AllPeriodFundFactorExposure.loc[ind,self.barra_ML_Return.columns.tolist()] = FundFactorExposure[0]
-                AllPeriodFundFactorExposure.loc[ind,'alpha'] = FundAlpha
+                AllPeriodFundFactorExposure.loc[ind,self.barra_ML_Return.columns.tolist()] = FundFactorExposure
+                AllPeriodFactorTValue.loc[ind,self.barra_ML_Return.columns.tolist()] = FactorTValue
+                AllPeriodFactorPValue.loc[ind,self.barra_ML_Return.columns.tolist()] = FactorPValue
                 AllPeriodScore.loc[ind,'score'] = score
-            return AllPeriodFundFactorExposure, AllPeriodScore
+
+            return AllPeriodFundFactorExposure, AllPeriodFactorTValue, AllPeriodFactorPValue, AllPeriodScore
--- BarraModel/test_PerformanceAttribute.py
+++ BarraModel/test_PerformanceAttribute.py
@@ -1,18 +1,121 @@
 from tqdm import tqdm
 import pandas as pd
+import numpy as np
+
 import pickle
 import warnings
 from BarraModel.FundPerformanceAttribute import FundPerformanceAttribute
-# warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore')
 rootPath = 'C:\\Users\\Lenovo\\Desktop\\毕设材料\\PerformanceAttributionViaMachineLearning\\'
+allFundNAV = pd.read_excel(rootPath+'data\\股票多头-私募基金筛选(按日期排序).xlsx',index_col = 0)
+barra_ML_Return = pd.read_excel(rootPath+'TimeSeriesModel\\factorReturnData\\Fama-French三因子模型（极简算法）日收益率（截至到20220331）.xlsx',index_col = 0)
+barra_ML_Return = barra_ML_Return.iloc[:,1:]
+
+# 0421 计算基金的超额收益
+# allFundAlphaDF = pd.DataFrame(columns = allFundNAV.columns)
+# allFundPvalueDF = pd.DataFrame(columns = ['Alpha','MKT','SMB','HML'],index = allFundNAV.columns)
+# allFundTvalueDF = pd.DataFrame(columns = ['Alpha','MKT','SMB','HML'],index = allFundNAV.columns)
+# allFundScore = pd.DataFrame(columns = ['R2'],index = allFundNAV.columns)
+# for fund in tqdm(allFundNAV.columns):
+#     currFundName = fund
+#     currFundNAV = allFundNAV.loc[:, fund]
+#     fundPA = FundPerformanceAttribute(barra_ML_Return, currFundNAV)
+#     results,alphaDF = fundPA.getExcessReturn()
+#     for time in alphaDF.index:
+#         allFundAlphaDF.loc[time,fund] = alphaDF.loc[time,'alpha']
+#     allFundPvalueDF.loc[fund,:] = results.pvalues
+#     allFundTvalueDF.loc[fund,:] = results.tvalues
+#     allFundScore.loc[fund,['R2']] = results.rsquared
+# allFundAlphaDF.to_csv(rootPath+'TimeSeriesModel\\FFModelResult\\fundAlpha.csv',encoding='utf_8_sig')
+# allFundPvalueDF.to_csv(rootPath+'TimeSeriesModel\\FFModelResult\\fundPvalue.csv',encoding='utf_8_sig')
+# allFundTvalueDF.to_csv(rootPath+'TimeSeriesModel\\FFModelResult\\fundTvalue.csv',encoding='utf_8_sig')
+# allFundScore.to_csv(rootPath+'TimeSeriesModel\\FFModelResult\\fundScore.csv',encoding='utf_8_sig')
+
+# 将基金的超额收益与我的因子收益率进行回归，看显著性
+allFundPvalueDF = pd.read_csv(rootPath+'TimeSeriesModel\\FFModelResult\\fundPvalue.csv',index_col = 0)
+allFundAlphaDF = pd.read_excel(rootPath+'TimeSeriesModel\\FFModelResult\\fundAlpha.xlsx',index_col = 0)
+analysisFundList = []
+for fund in allFundPvalueDF.index:
+    if allFundPvalueDF.loc[fund,'Alpha']<0.1:
+        analysisFundList.append(fund)
+analysisFundList = allFundNAV.columns.tolist()
+
+MLFactorReturn = pd.read_excel(rootPath+'backtest\\newFactorReturn\\LGBM Factor日频换仓无费率多空收益率0422.xlsx',index_col = 0)
+selectedFundPvalueDF = pd.DataFrame(index = analysisFundList)
+selectedFundTvalueDF = pd.DataFrame(index = analysisFundList)
+selectedFundScore = pd.DataFrame(index = analysisFundList)
+for fund in tqdm(analysisFundList):
+    currFundName = fund
+    currFundNAV = allFundAlphaDF.loc[:, fund]
+    fundPA = FundPerformanceAttribute(MLFactorReturn, currFundNAV, 2021, fund)
+    if fundPA.getExcessReturn() != 0:
+        results, alphaDF = fundPA.getExcessReturn()
+        selectedFundPvalueDF.loc[fund, 'pvalue'] = results.pvalues[1]
+        selectedFundTvalueDF.loc[fund, 'tvalue'] = results.tvalues[1]
+        selectedFundScore.loc[fund, 'R2'] = results.rsquared
+
+
 barra_ML_Return = pd.read_pickle(rootPath+'BarraModel\\barra_ML_Return.pickle')
+# 只取风格因子+机器学习因子部分做输入
+barraFactorList = ['Size','Beta','Momentum','ResidualVolatility','NonlinearSize','Book2Price','Liquidity','EarningsYield','Growth','Leverage','MLFactor']
+barra_ML_Return = barra_ML_Return.loc[:,barraFactorList]
 allFundNAV = pd.read_excel(rootPath+'data\\股票多头-私募基金筛选.xlsx',index_col = 0)
-for fund in allFundNAV.columns:
+AllPeriodFundFactorExposureDict,AllPeriodFactorTValueDict,AllPeriodFactorPValueDict,AllPeriodScoreDict = {},{},{},{}
+
+#对单一基金进行归因
+fund = allFundNAV.columns.tolist()[0]
+currFundName = fund
+currFundNAV = allFundNAV.loc[:, fund]
+fundPA = FundPerformanceAttribute(barra_ML_Return, currFundNAV)
+AllPeriodFundFactorExposure, AllPeriodFactorTValue, AllPeriodFactorPValue, AllPeriodScore = fundPA.getRollingFundExposure(
+    '年度')
+barraWithourMLFactorList = ['Size', 'Beta', 'Momentum', 'ResidualVolatility', 'NonlinearSize', 'Book2Price',
+                            'Liquidity', 'EarningsYield', 'Growth', 'Leverage']
+barra_Return = barra_ML_Return.loc[:, barraWithourMLFactorList]
+fundPA_withoutML = FundPerformanceAttribute(barra_Return, currFundNAV)
+newAllPeriodFundFactorExposure, newAllPeriodFactorTValue, newAllPeriodFactorPValue, newAllPeriodScore = fundPA_withoutML.getRollingFundExposure(
+    '年度')
+AllPeriodScoreCompare = AllPeriodScore
+AllPeriodScoreCompare.loc[:,'Score Without ML'] = np.array(newAllPeriodScore)
+
+# 所有的基金统一进行归因
+for fund in tqdm(allFundNAV.columns):
     currFundName = fund
     currFundNAV = allFundNAV.loc[:,fund]
     fundPA = FundPerformanceAttribute(barra_ML_Return,currFundNAV)
-    AllPeriodFundFactorExposure, AllPeriodScore = fundPA.getRollingFundExposure('季度')
+    AllPeriodFundFactorExposure, AllPeriodFactorTValue, AllPeriodFactorPValue, AllPeriodScore = fundPA.getRollingFundExposure('年度')
     if type(AllPeriodFundFactorExposure) != str:
-        AllPeriodFundFactorExposure.to_csv(rootPath+'analysis\\'+fund+'业绩归因.csv',encoding = 'utf_8_sig')
-        # 0416请无比搞清楚score到底是干嘛的，如何验证每个因子的有效性
-        AllPeriodScore.to_csv(rootPath+'analysis\\'+fund+'Score.csv',encoding = 'utf_8_sig')
+        AllPeriodFundFactorExposureDict[fund] = AllPeriodFundFactorExposure
+        AllPeriodFactorTValueDict[fund] = AllPeriodFactorTValue
+        AllPeriodFactorPValueDict[fund] = AllPeriodFactorPValue
+        AllPeriodScoreDict[fund] = AllPeriodScore
+        # AllPeriodFundFactorExposure.to_csv(rootPath+'analysis\\'+fund+'业绩归因.csv',encoding = 'utf_8_sig')
+        # 只取风格因子+机器学习因子部分做输入
+    barraWithourMLFactorList = ['Size','Beta','Momentum','ResidualVolatility','NonlinearSize','Book2Price','Liquidity','EarningsYield','Growth','Leverage']
+    barra_Return = barra_ML_Return.loc[:,barraWithourMLFactorList]
+    fundPA_withoutML = FundPerformanceAttribute(barra_Return, currFundNAV)
+    newAllPeriodFundFactorExposure, newAllPeriodFactorTValue, newAllPeriodFactorPValue, newAllPeriodScore = fundPA.getRollingFundExposure('年度')
+
+writer1 = pd.ExcelWriter(rootPath + 'analysis\\私募基金业绩归因Exposure.xlsx', engine='openpyxl')
+for fund in AllPeriodFundFactorExposureDict:
+    AllPeriodFundFactorExposureDict[fund].to_excel(writer1,sheet_name=fund,encoding = 'utf_8_sig')
+writer1.save()
+writer1.close()
+
+writer2 = pd.ExcelWriter(rootPath + 'analysis\\私募基金业绩归因Score.xlsx', engine='openpyxl')
+for fund in AllPeriodScoreDict:
+    AllPeriodScoreDict[fund].to_excel(writer2,sheet_name=fund,encoding = 'utf_8_sig')
+writer2.save()
+writer2.close()
+
+writer3 = pd.ExcelWriter(rootPath + 'analysis\\私募基金业绩归因TValue.xlsx', engine='openpyxl')
+for fund in AllPeriodFactorTValueDict:
+    AllPeriodFactorTValueDict[fund].to_excel(writer3,sheet_name=fund,encoding = 'utf_8_sig')
+writer3.save()
+writer3.close()
+
+writer4 = pd.ExcelWriter(rootPath + 'analysis\\私募基金业绩归因PValue.xlsx', engine='openpyxl')
+for fund in AllPeriodFactorPValueDict:
+    AllPeriodFactorPValueDict[fund].to_excel(writer4,sheet_name=fund,encoding = 'utf_8_sig')
+writer4.save()
+writer4.close()
